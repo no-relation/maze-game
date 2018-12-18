7ca439aba116f9c2a12c55c1ab1ca73240a6733c
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-12-18 15:36:53 -0600

    created migration files

---
+++ server/db/migrate/20181218213251_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
