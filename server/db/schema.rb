8fcd3f216b932a6b80b7e3a4571940ae2b9ee809
no-relation <eddiec76@gmail.com>
2018-12-18 17:55:30 -0600

    added maze attributes

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_213613) do
+ActiveRecord::Schema.define(version: 2018_12_18_233522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,12 @@ ActiveRecord::Schema.define(version: 2018_12_18_213613) do
   end
 
   create_table "mazes", force: :cascade do |t|
-    t.string "layout"
-    t.string "high_score", array: true
+    t.string "layout", array: true
+    t.string "high_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rows"
+    t.integer "columns"
   end
 
   create_table "players", force: :cascade do |t|
