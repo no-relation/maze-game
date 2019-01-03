b901510f3243f1ddf279cf699f278b28e63538e8
no-relation <eddiec76@gmail.com>
2019-01-03 12:36:07 -0600

    error box renders on bad login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,39 +1,49 @@
 import React, { Component } from "react";
+import { Alert } from "react-bootstrap";
 
 export class Login extends Component {
+  
+  state = {
+    errorMessage: null
+  }
+
+  errorBox() {
+    let errorMessage = this.state.errorMessage
+    if (errorMessage) {
+      return (
+        <Alert bsStyle="danger" role="alert">
+          {errorMessage}
+        </Alert>
+      )    
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={e => this.login(e)}>
-        {/* <h3>Login</h3>
-        <div className="form-group">
-          <label className="control-label">Username</label>
-          <input
-            name="usernameInput"
-            placeholder="Enter username"
-            className="form-control"
-            type="text"
-          />
-        </div> */}
-        <div className="form-group">
-          <label className="control-label">Email</label>
-          <input
-            name="emailInput"
-            placeholder="Enter email"
-            className="form-control"
-            type="email"
-          />
-        </div>
-        <div className="form-group">
-          <label className="control-label">Password</label>
-          <input
-            name="passwordInput"
-            placeholder="Enter password"
-            className="form-control"
-            type="password"
-          />
-        </div>
-        <button className="btn btn-primary">Login</button>
-      </form>
+      <div>
+        {this.errorBox()}
+        <form onSubmit={e => this.login(e)}>
+          <div className="form-group">
+            <label className="control-label">Email</label>
+            <input
+              name="emailInput"
+              placeholder="Enter email"
+              className="form-control"
+              type="email"
+            />
+          </div>
+          <div className="form-group">
+            <label className="control-label">Password</label>
+            <input
+              name="passwordInput"
+              placeholder="Enter password"
+              className="form-control"
+              type="password"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
     );
   }
 
@@ -55,8 +65,8 @@ export class Login extends Component {
     })
       .then(res => res.json())
       .then(result => {
-        if (result === "email or password are incorrect") {
-          console.log(result.error)
+        if (result.error === "email or password are incorrect") {
+          this.setState({ errorMessage: result.error })
         } else {
           localStorage.setItem("token", result.token);
           localStorage.setItem("player", JSON.stringify(result.player));
