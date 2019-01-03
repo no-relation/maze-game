a936d18b0cc07083d84dea248193ea463f01c7b0
no-relation <eddiec76@gmail.com>
2019-01-03 17:05:45 -0600

    cleanup

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import { Game } from "./Game";
 import { Login } from "../components/Login";
