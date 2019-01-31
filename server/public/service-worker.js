ab119a5e82d66da25b7d91a9c7803704dfbec7dc
no-relation <eddiec76@gmail.com>
2019-01-31 17:40:09 -0600

    changed clientX/Y to pageX/Y

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ae09a747bd631b89812cd0edcd5b648.js"
+  "/precache-manifest.f1c69bc67f0fad35f9fc8edd2c233349.js"
 );
 
 workbox.clientsClaim();
